13d05c6614022c7e6ca6c12a2f6d562ab78e475f
crow-man <all.games.1847@gmail.com>
2024-01-17 13:55:11 +0330

    side bar menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,16 @@ let btnClose=document.querySelector('.basket-close-icon')
 
 btnBasket.addEventListener('click',function(){
     basket.classList.add('show-basket');
+    document.body.style.overflow='hidden';
 })
 btnClose.addEventListener('click',function(){
     basket.classList.remove('show-basket')
+    document.body.style.overflow='auto';
+})
+
+let btnsidebar=document.querySelector('.btn-sidebar')
+let sidebar=document.querySelector('.sidebar')
+
+btnsidebar.addEventListener('click',function(){
+    sidebar.classList.toggle('show-sidebar');
 })
